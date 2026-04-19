9b3e8c42c2e2d760e854501d9fc1c4c8001e23aa
Felipe Valenzuela <feelipevalenzuela@hotmail.com>
2026-04-19 20:10:21 -0300

    primeros avances

--- app/src/main/java/com/tp3/pruebaappfv/components/ButtonAction.kt
+++ app/src/main/java/com/tp3/pruebaappfv/components/ButtonAction.kt
@@ -1,3 +1,12 @@
 package com.tp3.pruebaappfv.components
 
-import
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun ButtonAction() {
+    Button(onClick = { /*TODO*/ }) {
+        Text(text = "Button")
+    }
+}
--- app/src/main/java/com/tp3/pruebaappfv/ui/theme/Type.kt
+++ app/src/main/java/com/tp3/pruebaappfv/ui/theme/Type.kt
@@ -1,11 +1,17 @@
 package com.tp3.pruebaappfv.ui.theme
 
+
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.tp3.pruebaappfv.R
 
+val  poppinFonts = FontFamily(
+    Font(R.font.poppins_semi_bold, FontWeight.SemiBold),
+)
 // Set of Material typography styles to start with
 val Typography = Typography(
     bodyLarge = TextStyle(
